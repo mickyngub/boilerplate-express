e383a0e1f02b102df40d88925d5d98dee195cf45
mickyngub <micky47561@gmail.com>
2020-06-02 09:47:06 +0000

    first until .env file

--- myApp.js
+++ myApp.js
@@ -9,22 +9,41 @@ var app = express();
 
 
 /** 1) Meet the node console. */
-
+console.log("Hello world");
 
 /** 2) A first working Express Server */
+// app.get("/", function(req,res) {
+//   res.send("Hello Express");
+// })
+
+/** 4) Serve static assets  */
+app.use("", express.static(__dirname + "/public"));
 
 
 /** 3) Serve an HTML file */
-
-
-/** 4) Serve static assets  */
+let absolutePath = __dirname + "/views/index.html";
+app.get("/", function(req,res) {
+  res.sendFile(absolutePath);
+});
 
 
 /** 5) serve JSON on a specific route */
-
+// app.get("/json", function(req, res) {
+//   res.json({ "message" : "Hello json"});
+// })
 
 /** 6) Use the .env file to configure the app */
- 
+process.env.MESSAGE_STYLE="uppercase";
+if(process.env.MESSAGE_STYLE === "uppercase") {
+  app.get("/json", function(req,res) {
+    res.json({"message": "HELLO JSON"});
+  });
+} else {
+  app.get("/json", function(req,res) {
+    res.json({"message": "Hello json"});
+  });
+}
+
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
